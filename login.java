08e55ce9c53a12085352a92b2d46f11eb10a00bd
shabbeer <shabbeermca2010@gmail.com>
2023-09-07 20:28:45 +0530

    happy

--- login.java
+++ login.java
@@ -1,5 +1,14 @@
-# Java Web App ResourceBundle properties file
-...
-webapp.ldap.username=secretUsername
-webapp.ldap.password=secretPassword
-...
+<script language="javascript">
+ 
+function loginPage(form) {
+if (form.userName.value=="username") { 
+if (form.passWord.value=="password") {              
+location="home.html" 
+} else {
+alert("Incorrect detail Password")
+}
+} else {  alert("Incorrect detail Username")
+}
+}
+ 
+</script>
